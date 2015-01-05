9411799b85b44eddcf173c1ec6820d8ca988d8c0
GregoireH <gregoire.heitz@epfl.ch>
2015-01-05 12:46:31 +0100

    finish updating LEQuqd example code

--- LEQuad/dfu-programming.bat
+++ LEQuad/dfu-programming.bat
@@ -1,5 +1,5 @@
 ..\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 erase
 ..\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 get
-..\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 flash Debug\MegaFly2.hex --suppress-bootloader-mem 
+..\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 flash Release\MegaFly2.hex --suppress-bootloader-mem 
 ..\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 reset
 #pause
