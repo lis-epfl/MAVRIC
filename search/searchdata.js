69b94bc87bdb53558aa1e2685659f61cb1874b0e
Julien Lecoeur <julien.lecoeur@epfl.ch>
2015-01-14 09:52:02 +0100

    Update doc

---
+++ search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghijklmnopqrstuvwxyz",
+  1: "_abcdfghijklmnopqrstuvw",
+  2: "acdlmstv",
+  3: "dglmprt",
+  4: "abcdefghijklmnopqrstuvwxyz",
+  5: "d",
+  6: "d",
+  7: "r",
+  8: "_acghms",
+  9: "m"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Pages"
+};
+
