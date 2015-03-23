a5d69d63b23ca1df99445e05248b95ca403fb7db
Julien Lecoeur <julien.lecoeur@epfl.ch>
2015-03-23 17:57:51 +0100

    Start resolving conflicts

--- LEQuad/src/central_data.c
+++ LEQuad/src/central_data.c
@@ -101,7 +101,8 @@ bool central_data_init()
 										&central_data.state,
 										&central_data.waypoint_handler,
 										&central_data.sim_model,
-										&central_data.remote);
+										&central_data.remote,
+										&central_data.joystick_parsing);
 	time_keeper_delay_ms(100);
 
 	// Init imu
