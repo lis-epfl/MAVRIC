c5bc560c15964aad157a4deb2a1aa6e470f6d152
Alexandre <alexandre.cherpillod@epfl.ch>
2015-08-25 13:44:15 +0200

    removed the Release folder from the tracking

--- LEQuad/Release/src/asf/avr32/utils/startup/trampoline_uc3.d
+++
@@ -1,7 +0,0 @@
-src/asf/avr32/utils/startup/trampoline_uc3.d \
- src/asf/avr32/utils/startup/trampoline_uc3.o \
- src/asf/avr32/utils/startup/trampoline_uc3.o: \
- ../src/asf/avr32/utils/startup/trampoline_uc3.S \
- ../src/asf/avr32/utils/startup/trampoline_uc3.h
-
-../src/asf/avr32/utils/startup/trampoline_uc3.h:
